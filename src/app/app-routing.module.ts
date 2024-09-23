0b790469dcf685f94ccfc1676a74fc9b257e9dda
valencora <coratolovalen@gmail.com>
2024-09-23 12:31:42 -0300

    la cosas que hice mientras los chicos se rascan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,29 @@ import { BookdetailComponent } from './pages/bookdetail/bookdetail.component';
 import { HomelistComponent } from './pages/homelist/homelist.component';
 import { LoanDetailComponent } from './pages/loan-detail/loan-detail.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { UsersComponent } from './pages/users/users.component';
+import { BookComponent } from './pages/book/book.component';
+import { AddBookComponent } from './pages/add-book/add-book.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { LibrarianComponent } from './pages/librarian/librarian.component';
+import { HomesinregComponent } from './pages/homesinreg/homesinreg.component';
 
 const routes: Routes = [
 {path: 'home', component: HomeComponent},
-{path: '', redirectTo: 'home', pathMatch: 'full'},
+{path: 'homesinreg', component: HomesinregComponent},
+{path: '', redirectTo: 'homesinreg', pathMatch: 'full'},
 {path: 'bookdetail', component: BookdetailComponent},
 {path: 'homelist', component: HomelistComponent},
 {path: 'loandetail', component: LoanDetailComponent},
 {path: 'profile', component: ProfileComponent},
+{path: 'users', component: UsersComponent},
+{path: 'books', component: BookComponent},
+{path: 'add-book', component: AddBookComponent},
+{path: 'login', component: LoginComponent},
+{path: 'admin', component: AdminComponent},
+{path: 'librarian', component: LibrarianComponent},
+{path: 'admin', component: AdminComponent},
 
 
 
