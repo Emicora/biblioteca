78cc7099ddd39102080acc42714a8f91f5751b6f
valencora <coratolovalen@gmail.com>
2024-09-24 20:53:18 -0300

    ,

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-nav',
+  selector: 'app-nav', 
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
 
+  constructor(private router: Router) {}
+
+  
+  navigateHome() {
+    const role = localStorage.getItem('role');
+  
+    if (role === 'admin') {
+      this.router.navigate(['/homeadmin']);
+    } else if (role === 'user') {
+      this.router.navigate(['/home']);
+    } else if (role === 'librarian') {
+      this.router.navigate(['/homeadmin']);
+    } else {
+      console.error('No role found in localStorage');
+    }
+  }
+  
 }
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
-
+export class ProfileComponent implements OnInit {
+  isAdmin: boolean = false; 
+  ngOnInit() {
+   
+    const role = localStorage.getItem('role');
+    
+   
+    if (role === 'admin') {
+      this.isAdmin = true;
+    }
+  }
 }
